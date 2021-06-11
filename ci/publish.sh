27e6b0a1d80c8d62cd1d3cc1c42d9a79f6b40a24
Ivan Krutov <vania-pooh@aerokube.com>
2021-06-11 07:16:22 +0300

    Added Github Actions to automatically publish packages

---
+++ ci/publish.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -e
+set -x
+
+die(){
+  echo "$1"
+  exit 1
+}
+
+NPM_PUBLISH_TAG="next"
+VERSION=$(node -e 'console.log(require("./package.json").version)')
+
+if [[ $1 == "release" ]]; then
+  if [[ "${VERSION}" == *-* ]]; then
+    die "not publishing pre-release version in release mode"
+  fi
+  NPM_PUBLISH_TAG="latest"
+elif [[ $1 == "snapshot" ]]; then
+  if [[ "${VERSION}" != *-* ]]; then
+    die "not publishing release version in snapshot mode"
+  fi
+
+  UPSTREAM_SHA=$(git ls-remote https://github.com/aerokube/cypress-moon --tags $(git rev-parse --abbrev-ref HEAD) | cut -f1)
+  CURRENT_SHA=$(git rev-parse HEAD)
+  if [[ "${UPSTREAM_SHA}" != "${CURRENT_SHA}" ]]; then
+    die "not the HEAD commit"
+  fi
+  NPM_PUBLISH_TAG="next"
+else
+  die "unknown argument: '$1'"
+fi
+
+npm publish --tag="${NPM_PUBLISH_TAG}"
